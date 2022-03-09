3d444e4359160d042faeb4df3d33229592be3ea7
Chathuratharushka <80171910+Chathuratharushka@users.noreply.github.com>
2022-03-09 10:17:52 +0530

    Sending product details and product photo from backend to frontend

--- sqlTesting.py
+++ sqlTesting.py
@@ -1,7 +1,8 @@
 import mysql.connector
 from flask import Flask, render_template
 from PIL import Image as PImage
-
+import base64
+from io import BytesIO
 db = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -14,30 +15,38 @@ cursor = db.cursor()
 cursor.execute(sql_Query)
 # getting all records from database prod_details table
 records = cursor.fetchall()
-print("Total number of rows in table: ", cursor.rowcount)
+#printing database row count
+#print("Total number of rows in table: ", cursor.rowcount)
 
-product = "watermelon"
+product = "Watermelon"  #to test
 prodChecker= False
-prodPrice= None
+
 prodWeight=None
 product_details=None
 img_Path=None
 
-
+#checking product with database
 for row in records:               
     if(product==row[1]):
         prodChecker=True
-        prodPrice=row[2]
+        product_details={'name':row[1],'productPrice':row[2], 'productQuantity':row[4]}
         img_Path=row[5]
+
+        
 #reading the selected image
 image = PImage.open(img_Path)
 #oppening the selected image
-image.show()
+#image.show()
 
 
+im_file = BytesIO()
+image.save(im_file, format="JPEG")
+im_bytes = im_file.getvalue()  #image in binary format.
+encoded_img_data = base64.b64encode(im_bytes)
 
+#print(type(encoded_img_data))
 if(prodChecker==True):
-    print("Product has found. \nProduct : "+product+"\nprice : Rs "+str(prodPrice))
+    print("Product has found. \nProduct : "+product+"\nprice : Rs ")
 else:
     print("Product has not found.")
 
@@ -50,4 +59,4 @@ app = Flask(__name__)
 @app.route('/home')
 def home_page():
     
-    return render_template('home.html', data=prodPrice)
+    return render_template('home.html', img_data=encoded_img_data.decode('utf-8'), data=product_details)
