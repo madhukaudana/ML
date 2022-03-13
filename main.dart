ebc932913a4cbb4a32b0ddb5cfc902629e7efdcf
Chathuratharushka <80171910+Chathuratharushka@users.noreply.github.com>
2022-03-13 23:09:57 +0530

    Front end using dart and flutter

---
+++ main.dart
@@ -0,0 +1,41 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  String photoPath = 'assets/watermelon.jpg';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String greetings='';
+  final response =  await http.get('http://127.0.0.1:5000/');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [Text(greetings)],
+        ),
+      ),
+    );
+  }
+}
